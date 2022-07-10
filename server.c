81fcca9e0dbdb4a5618a3ebb5723d9550b0e36b3
Rafael Araujo Chinaglia <chinaglia.rafa@gmail.com>
2022-07-10 20:35:24 -0300

    initial commit

---
+++ server.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, char **argv)
+{
+  printf("Hello, world.");
+}
